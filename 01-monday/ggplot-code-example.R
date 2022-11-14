4387ef85c2521ae0aa7b2092b921ad95a7fc72f8
Gavin Simpson <ucfagls@gmail.com>
2022-11-14 15:08:55 +0100

    update example

--- 01-monday/ggplot-code-example.R
+++ 01-monday/ggplot-code-example.R
@@ -1,28 +1,40 @@
 library("pangaear")
+library("janitor")
+library("dplyr"
+library("ggplot2")
 
+# load foram isotope data from Baffin Bay
 res <- pg_data(doi = "10.1594/PANGAEA.744844")
 
+# look at the format
 str(res, max = 1)
 
+# can extract and display the metadata
 res[[1]]$meta
 
+# extract the data for the data set
 baffin <- res[[1]]$data
 
+# what variables do we have?
 names(baffin)
 
+# rename the two isotope data columns and then clean the remaining names
 baffin <- baffin %>%
   rename(d18o = `N. pachyderma s δ18O [‰ PDB]`,
          d13c = `N. pachyderma s δ13C [‰ PDB]`) %>%
   janitor::clean_names() %>%
-  mutate(event = factor(event))
+  mutate(event = factor(event)) # convert event to a factor
 
+# how many samples per core site
 baffin %>%
 group_by(event) %>%
 count()
 
+# store some labels for the plot
 ylabel <- expression(delta^{18} * O ~ "[‰ VPDB]")
 xlabel <- "Depth [m]"
 
+# plot a single core
 baffin %>%
 filter(event == "HUD85-027-16") %>%
 ggplot(aes(y = d18o, x = depth_m)) +
@@ -31,6 +43,7 @@ ggplot(aes(y = d18o, x = depth_m)) +
   scale_x_reverse() +
   labs(y = ylabel, x = xlabel)
 
+# plot all cores but colour by `event`
 baffin %>%
 ggplot(aes(y = d18o, x = depth_m, colour = event)) +
   geom_point() +
@@ -38,6 +51,7 @@ ggplot(aes(y = d18o, x = depth_m, colour = event)) +
   scale_x_reverse() +
   labs(y = ylabel, x = xlabel)
   
+# plot all cores but facet by `event`, no colour
 baffin %>%
 ggplot(aes(y = d18o, x = depth_m)) +
   geom_point() +
@@ -46,6 +60,7 @@ ggplot(aes(y = d18o, x = depth_m)) +
   labs(y = ylabel, x = xlabel) +
   facet_wrap(~ event)
 
+# combine both colour and facetting
 baffin %>%
 ggplot(aes(y = d18o, x = depth_m, colour = event)) +
   geom_point() +
